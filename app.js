722ba675d5cb44026dd629ca60f9c731c94793be
indigorose <indigorose049@gmail.com>
2024-03-26 18:54:57 +0000

    Update changes

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ if (rword == word) {
 
 // Next we are looking at creating stacks using OOP
 
-let Stack = function () {
+var Stack = function () {
 	this.count = 0;
 	this.storage = {};
 
@@ -63,3 +63,42 @@ myStack.push(2);
 console.log(myStack.peek());
 console.log(myStack.pop());
 console.log(myStack.peek());
+
+// Sets
+
+function mySet() {
+	// the var collection will hold the set
+	var collection = [];
+	// this method will check for the presence of an element and return true or false
+	this.has = function (element) {
+		return collection.indexOf(element) !== -1;
+	};
+	// this method will return all the values in the set
+	this.values = function () {
+		return collection;
+	};
+	// this method will add an element to the set
+	this.add = function (element) {
+		if (!this.has(element)) {
+			collection.push(element);
+			return true;
+		}
+		return false;
+	};
+	this.remove = function (element) {
+		if (this.has(element)) {
+			index = collection.indexOf(element);
+			collection.splice(index, 1);
+			return true;
+		}
+		return false;
+	};
+	// this method will return the size of the collection
+	this.size = function () {
+		return collection.length;
+	};
+	// this method will return the union of the two sets
+	this.union = function (otherSet) {
+		let uni;
+	};
+}
